e0aaabf07c3784c39a8846859cfaca26b41c6fc8
aleksey2398 <87311512+aleksey2398@users.noreply.github.com>
2021-11-12 16:36:53 +0200

    next

---
+++ js/01-gallery.js
@@ -0,0 +1,50 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+console.log(galleryItems);
+
+
+const gallery = document.querySelector(".gallery");
+const galleryItem = createImgCard(galleryItems);
+
+gallery.insertAdjacentHTML("beforeend", galleryItem);
+
+function createImgCard(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description}) => {
+            return `
+  <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div
+`}).join('');
+}
+
+gallery.addEventListener("click", choosePic);
+
+function choosePic(event) {
+    event.preventDefault();
+
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+    const picture = (event.target.dataset.source);
+
+    const instance = basicLightbox.create(`
+     <img src="${picture}" width="800" height="600">
+`);
+
+instance.show()
+        
+    }
+    
+
+
+    
+
+
